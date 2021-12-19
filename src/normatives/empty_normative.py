f9350393fcfeed49a3bfaa54041a653ae47e3fae
Inaki <i.martin.soroa@gmail.com>
2021-12-19 15:18:16 +0100

    recover normatives.name property

--- src/normatives/empty_normative.py
+++ src/normatives/empty_normative.py
@@ -2,5 +2,9 @@ from components.box import Box
 
 
 class Normative:
+    @classmethod
+    def name(cls):
+        return cls.__name__
+
     def calculate_box(self, box: Box):
         raise NotImplementedError("You need to select a normative implementation first!")
